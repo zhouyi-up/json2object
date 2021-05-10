cf12ec40ca2dc1592b54f767618aabcac28af7f8
LiuJun <liujun@liu-jun.top>
2021-05-10 21:41:14 +0800

    UI

--- src/main/java/cn/jtools/json2object/anactions/TestAnAction.java
+++ src/main/java/cn/jtools/json2object/anactions/TestAnAction.java
@@ -2,15 +2,20 @@ package cn.jtools.json2object.anactions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiUtilBase;
+import org.jetbrains.annotations.NotNull;
 
 public class TestAnAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
-        WriteCommandAction.runWriteCommandAction(e.getProject(), ()-> {
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, e.getProject());
 
-        });
     }
 }
